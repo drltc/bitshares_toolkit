8729d8d208bd6ee051d0aeab2e1ff27e3902bc8f
Nikolai Mushegian <nikolai@bitshares.org>
2014-09-18 15:23:46 -0400

    restore dilution

--- libraries/blockchain/chain_database.cpp
+++ libraries/blockchain/chain_database.cpp
@@ -601,67 +601,22 @@ namespace bts { namespace blockchain {
             pending_base_record->collected_fees -= max_available_paycheck;
             pending_state->store_asset_record( *pending_base_record );
 
-            delegate_record->delegate_info->pay_balance += accepted_paycheck;
-            delegate_record->delegate_info->votes_for += accepted_paycheck;
+            auto max_shares = pending_base_record->max_share_supply;
+            auto current_shares = pending_base_record->current_share_supply;
+            auto max_subsidy = (max_shares - current_shares) / P2P_DILUTION_RATE;
+            auto accepted_subsidy = (max_subsidy * pay_rate_percent) / 100;
+            auto burned_subsidy = max_subsidy - accepted_subsidy;
+
+            delegate_record->delegate_info->pay_balance += accepted_paycheck + accepted_subsidy;
+            delegate_record->delegate_info->votes_for += accepted_paycheck + accepted_subsidy;
             pending_state->store_account_record( *delegate_record );
 
             auto base_asset_record = pending_state->get_asset_record( asset_id_type(0) );
             FC_ASSERT( base_asset_record.valid() );
             base_asset_record->current_share_supply -= (max_available_paycheck - accepted_paycheck);
+            base_asset_record->current_share_supply += accepted_subsidy;
+            base_asset_record->max_share_supply -= burned_subsidy;
             pending_state->store_asset_record( *base_asset_record );
-
-
-
-            /*   For my own notes... delete ASAP
-            auto delegate_record = pending_state->get_account_record( self->get_delegate_record_for_signee( block_signee ).id );
-            FC_ASSERT( delegate_record.valid() && delegate_record->is_delegate() );
-<<<<<<< HEAD
-            const auto pay_percent = delegate_record->delegate_info->pay_rate;
-            FC_ASSERT( pay_percent <= 100 );
-            const auto pending_pay = pending_state->get_delegate_pay_rate();
-            const auto pay = ( pay_percent * pending_pay ) / 100;
-
-            const auto prev_accumulated_fees = pending_state->get_accumulated_fees();
-#ifndef WIN32
-#warning [HARDFORK] This will hardfork BTSX by changing how accumulated fees are calculated
-#endif
-            //pending_state->set_accumulated_fees( prev_accumulated_fees - pay );
-            pending_state->set_accumulated_fees( prev_accumulated_fees - pending_pay );
-
-            auto base_asset_record = pending_state->get_asset_record( asset_id_type(0) );
-
-            const auto pending_subsidy_pay = (base_asset_record->maximum_share_supply - base_asset_record->current_share_supply) / P2P_DILUTION_RATE;
-            const auto subsidy_pay = (pay_percent * pending_subsidy_pay) / 100;
-
-            delegate_record->delegate_info->pay_balance += pay + subsidy_pay;
-            delegate_record->delegate_info->votes_for += pay + subsidy_pay;
-=======
-            const auto pay_rate_percent = delegate_record->delegate_info->pay_rate;
-            FC_ASSERT( pay_rate_percent >= 0 && pay_rate_percent <= 100 );
-            const auto max_available_paycheck = pending_state->get_delegate_pay_rate();
-            const auto accepted_paycheck = ( pay_rate_percent * max_available_paycheck ) / 100;
-
-            auto pending_base_record = pending_state->get_asset_record( asset_id_type( 0 ) );
-            FC_ASSERT( pending_base_record.valid() );
-            pending_base_record->collected_fees -= max_available_paycheck;
-            pending_state->store_asset_record( *pending_base_record );
-
-            delegate_record->delegate_info->pay_balance += accepted_paycheck;
-            delegate_record->delegate_info->votes_for += accepted_paycheck;
->>>>>>> 8c99991dfebf86298daafb2333e5f86231679e31
-            pending_state->store_account_record( *delegate_record );
-
-            FC_ASSERT( base_asset_record.valid() );
-<<<<<<< HEAD
-            base_asset_record->current_share_supply -= (pending_pay - pay);
-            base_asset_record->current_share_supply += subsidy_pay;
-            base_asset_record->maximum_share_supply -= (pending_subsidy_pay - subsidy_pay);
-=======
-            base_asset_record->current_share_supply -= (max_available_paycheck - accepted_paycheck);
->>>>>>> 8c99991dfebf86298daafb2333e5f86231679e31
-            pending_state->store_asset_record( *base_asset_record );
-
-*/
 
       } FC_RETHROW_EXCEPTIONS( warn, "", ("block_id",block_id) ) }
 
