99c3ca3457e0cf99b14303e4636a337385d5e911
Vikram Rajkumar <vikram@bitshares.org>
2014-09-29 15:39:30 -0400

    Bump chain version to match release

--- libraries/blockchain/include/bts/blockchain/config.hpp
+++ libraries/blockchain/include/bts/blockchain/config.hpp
@@ -12,8 +12,8 @@
 /** @file bts/blockchain/config.hpp
  *  @brief Defines global constants that determine blockchain behavior
  */
-#define BTS_BLOCKCHAIN_VERSION                              109
-#define BTS_BLOCKCHAIN_DATABASE_VERSION                     138
+#define BTS_BLOCKCHAIN_VERSION                              110
+#define BTS_BLOCKCHAIN_DATABASE_VERSION                     139
 
 /**
  *  The address prepended to string representation of
