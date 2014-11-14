333f52fedf2abbf9b82e1b324ac6c748b0e5ebd2
drltc <drltc@users.noreply.github.com>
2014-11-14 11:19:09 -0500

    wip Some more implementation

--- tests/drltc_tests/py/bts_tests/__main__.py
+++ tests/drltc_tests/py/bts_tests/__main__.py
@@ -6,45 +6,108 @@
 
 import io
 import json
+import os
 import subprocess
 
-def create_genesis_file():
-    # programs/utils/bts_create_key --count=101 --seed=test-delegate-
-    keydata = subprocess.check_output(
-        [
-         "programs/utils/bts_create_key",
-         "--count=200",
-         "--seed=testkey-",
-        ],
-        )
-    print("here is the key data:")
-    key = json.loads(keydata.decode())
+class TestFixture(object):
+    def __init__(self):
+        self.genesis_filename = "genesis.json"
+        self.basedir = "tmp"
+        return
 
-    balances = [[key[i]["pts_address"], 100000000000] for i in range(101)]
-    
-    genesis_json = {
-      "timestamp" : "2014-11-13T15:00:00",
-      
-      "market_assets" : [],
-      
-      "names" :
-      [
-        {
-          "name" : "init"+str(i),
-          "owner" : key[i]["public_key"],
-          "delegate_pay_rate" : 1,
+    def create_genesis_file(self):
+        # programs/utils/bts_create_key --count=101 --seed=test-delegate-
+        keydata = subprocess.check_output(
+            [
+             "programs/utils/bts_create_key",
+             "--count=200",
+             "--seed=testkey-",
+            ],
+            )
+        print("here is the key data:")
+        key = json.loads(keydata.decode())
+
+        balances = [[key[i]["pts_address"], 100000000000] for i in range(101)]
+        
+        genesis_json = {
+          "timestamp" : "2014-11-13T15:00:00",
+          
+          "market_assets" : [],
+          
+          "names" :
+          [
+            {
+              "name" : "init"+str(i),
+              "owner" : key[i]["public_key"],
+              "delegate_pay_rate" : 1,
+            }
+            for i in range(101)
+          ],
+
+          "balances" : balances,
         }
-        for i in range(101)
-      ],
+        
+        bts_sharedrop = []
 
-      "balances" : balances,
-    }
-    
-    bts_sharedrop = []
+        with open(self.get_genesis_path(), "w") as f:
+            json.dump(genesis_json, f, indent=4)
+        return
 
-    return genesis_json
+    def get_genesis_path(self):
+        return os.path.join(self.basedir, self.genesis_filename)
 
-import sys
+    def launch(self, client_count):
+        for clientnum in range(client_count):
+            Node(clientnum).launch()
+        return
 
-with open("genesis.json", "w") as f:
-    json.dump(create_genesis_file(), f, indent=4)
+class Node(object):
+    def __init__(self,
+        clientnum,
+        basedir="tmp",
+        genesis_filename="genesis.json",
+        ):
+
+        self.clientnum = clientnum
+        self.basedir = basedir
+        self.genesis_filename = genesis_filename
+        self.httpport = 9100+clientnum
+        self.p2pport = 9200+clientnum
+        self.rpcport = 9300+clientnum
+        #self.csport = 9400+clientnum
+        self.csport = None
+        return
+
+    def get_data_dir(self):
+        return os.path.join(self.basedir, self.client_num)
+
+    def get_genesis_path(self):
+        return os.path.join(self.basedir, self.genesis_filename)
+
+    def launch(self):
+        args = [
+         "programs/client/bitshares_client",
+         "--data-dir", self.get_data_dir(),
+         "--genesis-config", self.get_genesis_path(),
+         "--min-delegate-connection-count", "0",
+         "--server",
+         "--rpc-user", "user",
+         "--rpc-password", "pass",
+        ]
+        if self.httpport is not None:
+            args.extend(["--httpport", str(self.httpport)])
+        if self.p2pport is not None:
+            args.extend(["--p2p-port", str(self.p2pport)])
+        if self.rpcport is not None:
+            args.extend(["--rpcport", str(self.rpcport)])
+        if self.csport is not None:
+            args.extend(["--chain-server-port", str(self.csport)])
+        if self.clientnum > 0:
+            args.extend(["--connect-to", "127.0.0.1:9200"])
+        print(os.getcwd())
+        print(args)
+        return
+
+tf = TestFixture()
+tf.create_genesis_file()
+tf.launch(2)
