8499db7182b0aab0ec75b2d24c29353b0bd5f613
drltc <drltc@users.noreply.github.com>
2014-11-19 15:19:13 -0500

    Try with False

--- tests/drltc_tests/py/bts_tests/__main__.py
+++ tests/drltc_tests/py/bts_tests/__main__.py
@@ -141,7 +141,7 @@ class TestFixture(object):
             n = self.node[self.delegate2nodeid[i]]
             yield n.run_cmd("wallet_import_private_key",
                 self.name2privkey["init"+str(i)],
-                True,
+                False,
                 True,
                 )
             yield n.run_cmd("wallet_delegate_set_block_production",
